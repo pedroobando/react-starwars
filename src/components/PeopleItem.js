6a244e05c01ac451923e357cb2cd7154814742a4
pedroobando <pedroobando@hotmail.com>
2020-08-17 07:23:21 -0400

    useQuery Hook

--- src/components/PeopleItem.js
+++ src/components/PeopleItem.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-export const PeopleItem = ({ name, genre, birth_year }) => {
+export const PeopleItem = ({ name, gender, birth_year }) => {
   return (
     <div className="col-12 col-sm-6 col-md-4 col-lg-3">
       <div className="card mb-1">
         <div className="card-body">
           <h3 className="card-title text-warning">{name}</h3>
-          <p className="card-text text-muted">Genero:&nbsp;&nbsp;{genre}</p>
-          <p className="card-text text-muted">Cumpleanos:&nbsp;&nbsp;{birth_year}</p>
+          <p className="card-text text-muted">Genero:&nbsp;&nbsp;{gender}</p>
+          <p className="card-text text-muted">
+            Nacimiento / Creacion:&nbsp;&nbsp;{birth_year}
+          </p>
         </div>
       </div>
     </div>
